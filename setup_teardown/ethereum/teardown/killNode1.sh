1120b88eb9fe22ccafb583a26f8fca2eb90b4601
Tim Strauss <tim.strauss13@googlemail.com>
2020-06-25 12:21:59 +0200

    Fix and test other epoche

--- setup_teardown/ethereum/teardown/killNode1.sh
+++ setup_teardown/ethereum/teardown/killNode1.sh
@@ -2,5 +2,5 @@
 bootnodepid=$(lsof -i :30310 | sed -n 2p | cut -d ' ' -f2)
 nodepid=$(lsof -i :8545 | sed -n 2p | cut -d ' ' -f5)
 kill $nodepid
-kill bootnodepid
+kill $bootnodepid
 rm -rf ../setup/blockChainNode/node
