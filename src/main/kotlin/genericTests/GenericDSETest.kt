79745042cfb9c5c694a0d720dacbcf79d779f1c3
Tim Strauß <tim.strauss13@googlemail.com>
2020-06-26 12:01:24 +0200

    Correct Timeout Error

--- src/main/kotlin/genericTests/GenericDSETest.kt
+++ src/main/kotlin/genericTests/GenericDSETest.kt
@@ -15,9 +15,9 @@ fun main() {
 
     con.session().execute("CREATE TABLE tim_space.generics (uuid text PRIMARY KEY, boolvar boolean, intvar int, stringvar text);")
 
+    con.closeSession()
+
     DSEBoolTests.run(2)
     DSEIntTests.run(2)
     DSEStringTests.run(2)
-
-    con.closeSession()
 }
