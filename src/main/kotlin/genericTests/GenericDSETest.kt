dec0016119064bbe074377ad272d460842b6a176
Tim Strauß <tim.strauss13@googlemail.com>
2020-06-26 11:55:49 +0200

    Correct Generic Tests

--- src/main/kotlin/genericTests/GenericDSETest.kt
+++ src/main/kotlin/genericTests/GenericDSETest.kt
@@ -4,13 +4,20 @@ package genericTests
 
 import connectionDetails.CassandraConnectionDetails
 import connectionDetails.CassandraUtils
+import genericTests.dse.DSEBoolTests
+import genericTests.dse.DSEIntTests
+import genericTests.dse.DSEStringTests
 
 fun main() {
     CassandraUtils.createNewUser("tim", "abc", 2)
     val con = CassandraConnectionDetails("localhost", 9042, "tim", "abc")
     con.openSession()
 
-    con.session().execute("CREATE TABLE tim_space.generics (uuid string PRIMARY KEY, boolvar boolean, intvar int, stringvar text);")
+    con.session().execute("CREATE TABLE tim_space.generics (uuid text PRIMARY KEY, boolvar boolean, intvar int, stringvar text);")
+
+    DSEBoolTests.run(2)
+    DSEIntTests.run(2)
+    DSEStringTests.run(2)
 
     con.closeSession()
 }
--- src/main/kotlin/genericTests/GenericEthTest.kt
+++ src/main/kotlin/genericTests/GenericEthTest.kt
@@ -5,62 +5,10 @@ package genericTests
 import genericTests.ethereum.EthereumBoolTests
 import genericTests.ethereum.EthereumIntTests
 import genericTests.ethereum.EthereumStringTests
-import java.net.Socket
-import java.util.concurrent.TimeUnit
 
 fun main() {
-    var ganache = setUpGanache()
     EthereumBoolTests.run(2)
-    stopGanache(ganache)
-    ganache = setUpGanache()
     EthereumIntTests.run(2)
-    stopGanache(ganache)
-    ganache = setUpGanache()
     EthereumStringTests.run(2)
-    stopGanache(ganache)
-}
-
-fun stopGanache(ganacheProcess: Process) {
-    ganacheProcess.destroy()
-
-    try{
-        ganacheProcess.waitFor(30000, TimeUnit.MILLISECONDS)
-    } catch (e: Exception){
-        throw Exception("Blockchain was not stopped")
-    }
-}
-
-fun setUpGanache(): Process {
-    val isWindows = System.getProperty("os.name").startsWith("Windows")
-    val ganacheBuilder = ProcessBuilder()
-    if (isWindows) {
-        ganacheBuilder.command("cmd.exe",
-            "/c",
-            "ganache-cli " +
-                    "                           -l 47000000000" +
-                    "                           -g 0")
-    } else {
-        ganacheBuilder.command("bash",
-            "-c",
-            "ganache-cli " +
-                    "                           -l 47000000000" +
-                    "                           -g 0")
-    }
-    val ganacheProcess = ganacheBuilder.start()
-
-    checkBlockchainRunning(8545, 300)
-
-    return ganacheProcess
-}
-
-fun checkBlockchainRunning(port: Int, retries: Int) {
-    if (retries > 0) {
-        try {
-            Socket("localhost", port)
-        } catch (e: Exception) {
-            Thread.sleep(100)
-            checkBlockchainRunning(port, retries - 1)
-        }
-    }
 }
 
