9957dae4b2e5473b8e258485c755c6b219c4f715
Tim Strauss <tim.strauss13@googlemail.com>
2020-07-02 12:06:59 +0200

    Add Paxos for Array

--- src/main/kotlin/genericTests/dse/DSEArrayTests.kt
+++ src/main/kotlin/genericTests/dse/DSEArrayTests.kt
@@ -38,7 +38,7 @@ object DSEArrayTests {
         override fun testFunc(): Boolean {
             id = UUID.randomUUID().toString()
             return try {
-                session.execute("INSERT INTO tim_space.genericsArray (id, uuid, intvar) VALUES ($id, '$uuid', ${setValue as Int});").wasApplied()
+                session.execute("INSERT INTO tim_space.genericsArray (id, uuid, intvar) VALUES ($id, '$uuid', ${setValue as Int}) IF NOT EXISTS;").wasApplied()
             } catch (e: Exception) { false }
         }
 
@@ -47,7 +47,7 @@ object DSEArrayTests {
                 var success = false
                 while(!success) {
                     try {
-                        session.execute("DELETE FROM tim_space.genericsArray WHERE id = $id AND uuid = '$uuid' AND intvar = ${setValue as Int};")
+                        session.execute("DELETE FROM tim_space.genericsArray WHERE id = $id AND uuid = '$uuid' AND intvar = ${setValue as Int} IF EXISTS;")
                         success = true
                     } catch (e: Exception) {  }
                 }
@@ -63,7 +63,7 @@ object DSEArrayTests {
             return try {
                 id = session.execute("SELECT id FROM tim_space.genericsArray WHERE uuid = '$uuid' AND intvar = ${setValue as Int} LIMIT 1;").one()?.getUuid(0)
                     ?: return false
-                session.execute("DELETE FROM tim_space.genericsArray WHERE id = $id AND uuid = '$uuid' AND intvar = ${setValue as Int};").wasApplied()
+                session.execute("DELETE FROM tim_space.genericsArray WHERE id = $id AND uuid = '$uuid' AND intvar = ${setValue as Int}  IF EXISTS;").wasApplied()
                 true
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -76,7 +76,7 @@ object DSEArrayTests {
                 var success = false
                 while(!success) {
                     try {
-                        session.execute("INSERT INTO tim_space.genericsArray (id, uuid, intvar) VALUES ($id, '$uuid', ${setValue as Int});")
+                        session.execute("INSERT INTO tim_space.genericsArray (id, uuid, intvar) VALUES ($id, '$uuid', ${setValue as Int}) IF NOT EXISTS;")
                         success = true
                     } catch (e: Exception) {  }
                 }
@@ -91,6 +91,7 @@ object DSEArrayTests {
                 session.execute("SELECT intvar FROM tim_space.genericsArray WHERE uuid = '$uuid'").all()
                 true
             } catch (e: Exception) {
+                e.printStackTrace()
                 false
             }
         }
