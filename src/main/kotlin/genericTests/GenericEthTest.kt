3b4f24f28f7f7d6ae8119dfbbe92abcd999e9e48
Tim Strauss <tim.strauss13@googlemail.com>
2020-07-08 12:57:18 +0200

    Test eth

--- src/main/kotlin/genericTests/GenericEthTest.kt
+++ src/main/kotlin/genericTests/GenericEthTest.kt
@@ -6,21 +6,24 @@ import connectionDetails.EthereumConnectionDetails
 import connectionDetails.EthereumContractGasProvider
 import de.hpi.cc.datasource.decentralized.ethereum.smartcontracts.Generic
 import genericTests.ethereum.*
+import java.math.BigInteger
 
 fun main() {
     val con = EthereumConnectionDetails("http://${TestInfo.nodeHost}:8545")
     val generics = mutableListOf<String>()
     for (i in 0 until TestInfo.threads) {
-        generics.add(Generic.deploy(con.web3j, TestInfo.getEthTransactionManager(con.web3j, con.credentials), EthereumContractGasProvider()).send().contractAddress)
+        val contract = Generic.deploy(con.web3j, TestInfo.getEthTransactionManager(con.web3j, con.credentials), EthereumContractGasProvider()).send()
+        generics.add(contract.contractAddress)
+        contract.setInts(MutableList(221) { BigInteger.valueOf(it.toLong()) })
     }
-    EthereumBoolTests.run(TestInfo.threads, generics)
+ /*   EthereumBoolTests.run(TestInfo.threads, generics)
     println("bool done")
     EthereumIntTests.run(TestInfo.threads, generics)
     println("int done")
     EthereumStringTests.run(TestInfo.threads, generics)
     println("string done")
     EthereumBoolMappingTests.run(TestInfo.threads, generics)
-    println("mapping done")
+    println("mapping done")*/
     EthereumArrayTests.run(TestInfo.threads, generics)
     println("array done")
 }
